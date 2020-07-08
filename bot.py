a0eb7b72fd500c4ffbb0da8240f1d43fd2815edb
Joaquín Puche <lu4ypj@pm.me>
2020-07-08 19:14:04 -0300

    ¡VERSIÓN 1.0!
    Se añadió la librería minestat (de nuevo).
    Se añadió el loop que avisa de cambios en el estado del servidor.
    Se añadió el comando $callate, que apaga el bot.
    Se añadieron los jugadores activos al comando $estado.

--- bot.py
+++ bot.py
@@ -3,21 +3,53 @@ import discord
 import constantes
 import asyncio
 import paramiko
-import requests
+import minestat
 from discord.ext.commands import Bot
 
 client = Bot(command_prefix=constantes.PREFIX)
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-VERSION = '0.9.5'  # Falta el poder loopear los mensajes de info en #on-off para la 1.0.0
+servidor = minestat.MineStat(constantes.IP, 25565)
+VERSION = '1.0'
+estaba_on = True
+
+
+def establecer_estaba_on(valor):
+	global estaba_on
+	estaba_on = valor
+
+
+def obtener_estaba_on():
+	global estaba_on
+	return estaba_on
+
+
+def footer_embed(context):
+	return 'Comando solicitado por {}. \nDSMPBot Ver: {}'.format(str(context.author), VERSION)
+
+
+async def ver_estado_servidor_loop(canal_on_off):
+	while True:
+		if servidor.motd is not None:
+			if not obtener_estaba_on():
+				await canal_on_off.send('¡El servidor está ON!')
+				establecer_estaba_on(True)
+		else:
+			if obtener_estaba_on():
+				await canal_on_off.send('¡El servidor está OFF!')
+				establecer_estaba_on(False)
+		
+		await asyncio.sleep(6)
 
 
 @client.event
 async def on_ready():
+	canal_on_off = client.get_channel(constantes.CANAL_ON_OFF)
+	client.loop.create_task(ver_estado_servidor_loop(canal_on_off))
 	await client.change_presence(activity=discord.Game('DSMP - $comandos'))
 	print('-----------------------------------------------')
-	print('Logueado como ' + client.user.name)
-	print('Versión de Discord.py: ', discord.__version__)
+	print('Logueado como {}'.format(client.user))
+	print('Versión de Discord.py: {}'.format(discord.__version__))
 	print('-----------------------------------------------')
 
 
@@ -27,12 +59,12 @@ async def reiniciar(context):
 		try:
 			ssh.connect(constantes.SSH_HOST, port=22, username=constantes.SSH_USER, password=constantes.SSH_PASS)
 			ssh.exec_command('systemctl restart dogesthetic')
-
+			
 			estado_embed = discord.Embed(
 				description='{} está reiniciando el servidor.'.format(context.author),
 				color=0x00FF00
 			)
-
+			
 			await context.message.channel.send(embed=estado_embed)
 		finally:
 			ssh.close()
@@ -55,41 +87,47 @@ async def estado(context):
 		ssh.connect(constantes.SSH_HOST, port=22, username=constantes.SSH_USER, password=constantes.SSH_PASS)
 		(sshin, sshout, ssherr) = ssh.exec_command('systemctl status dogesthetic | grep -i Active')
 		resultado = sshout.read().decode('utf8').strip()
-
-		resultado_sv = requests.get("https://mcapi.us/server/status?ip={}".format(constantes.IP))
-		info_sv = resultado_sv.json()
-
+		
 		# Mostrar datos
 		estado_servicio = 'INACTIVO'
 		estado_servidor = 'INACTIVO'
-
+		estado_jugadores = 0
+		
 		if 'running' in resultado:
 			estado_servicio = 'ACTIVO'
-
-		if info_sv['online']:
+		
+		if servidor.version is not None:
 			estado_servidor = 'ACTIVO'
-
+		
+		if servidor.current_players is not None:
+			estado_jugadores = servidor.current_players
+		
 		if estado_servidor == 'ACTIVO':
 			color_embed = 0x00FF00
 		else:
 			color_embed = 0xCC0000
-
+		
 		estado_embed = discord.Embed(color=color_embed)
 		estado_embed.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
-
+		
 		estado_embed.add_field(
 			name='Estado del servicio:',
 			value=estado_servicio,
 			inline=False
 		)
-
+		
 		estado_embed.add_field(
 			name='Estado del servidor:',
 			value=estado_servidor,
 			inline=False
 		)
-
-		estado_embed.set_footer(text='Comando solicitado por ' + str(context.author))
+		
+		estado_embed.add_field(
+			name='Jugadores activos:',
+			value=estado_jugadores
+		)
+		
+		estado_embed.set_footer(text=footer_embed(context))
 		await context.message.channel.send(embed=estado_embed)
 	finally:
 		ssh.close()
@@ -141,25 +179,23 @@ async def reglas(context):
 	)
 	reglas_embed.add_field(
 		name='Regla 9:',
-		value='No destruir ni dañar de ninguna manera las construcciones de los demás.',
-		inline=False
+		value='No destruir ni dañar de ninguna manera las construcciones de los demás.'
 	)
-	reglas_embed.set_footer(text='Comando solicitado por ' + str(context.author))
+	reglas_embed.set_footer(text=footer_embed(context))
 	await context.message.channel.send(embed=reglas_embed)
 
 
 @client.command(name='sobredsmp', pass_context=True)
 async def sobredsmp(context):
 	infodsmp = discord.Embed(
-		description='Dogesthetic SMP es un servidor survival en el que se mezcla el ámbito '
-		'técnico, estético y sobre todo cooperativo. Es un servidor en el que se '
-		'pueden hacer muchos proyectos y experimentos, este es un servidor serio y por '
-		'esta razón se necesita rellenar un formulario para entrar. El formulario lo '
-		'puedes encontrar en <#437781641201451010>, donde encontrarás instrucciones.',
+		description='Dogesthetic SMP es un servidor survival en el que se mezcla el ámbito técnico, estético y sobre '
+		'todo cooperativo. Es un servidor en el que se pueden hacer muchos proyectos y experimentos, '
+		'este es un servidor serio y por esta razón se necesita rellenar un formulario para entrar. El '
+		'formulario lo puedes encontrar en <#437781641201451010>, donde encontrarás instrucciones.',
 		color=0x00FF00
 	)
 	infodsmp.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
-	infodsmp.set_footer(text='Comando solicitado por ' + str(context.author))
+	infodsmp.set_footer(text=footer_embed(context))
 	await context.message.channel.send(embed=infodsmp)
 
 
@@ -169,10 +205,9 @@ async def ip(context):
 	ip_embed.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
 	ip_embed.add_field(
 		name='Para obtener la ip:',
-		value='<#483373646253916171>',
-		inline=False
+		value='<#483373646253916171>'
 	)
-	ip_embed.set_footer(text='Comando solicitado por ' + str(context.author))
+	ip_embed.set_footer(text=footer_embed(context))
 	await context.message.channel.send(embed=ip_embed)
 
 
@@ -202,10 +237,9 @@ async def comandos(context):
 	)
 	comandos_embed.add_field(
 		name='Estado del servidor:',
-		value='$estado',
-		inline=False
+		value='$estado'
 	)
-	comandos_embed.set_footer(text='Comando solicitado por ' + str(context.author))
+	comandos_embed.set_footer(text=footer_embed(context))
 	await context.message.channel.send(embed=comandos_embed)
 
 
@@ -230,12 +264,28 @@ async def version(context):
 	)
 	version_embed.add_field(
 		name='Actualmente hosteado en:',
-		value='{} {}'.format(platform.system(), platform.release()),
-		inline=False
+		value='{} {}'.format(platform.system(), platform.release())
 	)
 	version_embed.add_field(name='Repositorio de GitHub:', value='<https://github.com/elJoa/DSMPBot>')
-	version_embed.set_footer(text='Comando solicitado por ' + str(context.author))
+	version_embed.set_footer(text=footer_embed(context))
 	await context.message.channel.send(embed=version_embed)
+
+
+@client.command(name='callate', pass_context=True)
+async def callate(context):
+	if context.message.author.id in constantes.ADMINISTRADORES:
+		await context.message.channel.send('Okey, okey. Me callo. :(')
+		await client.close()
+	else:
+		await context.message.delete()
+		error = discord.Embed(
+			title='¡Vaya!',
+			description='¡No tienes permisos para usar este comando, pequeño curioso!',
+			color=0xCC0000
+		)
+		mensaje = await context.message.channel.send(embed=error)
+		await asyncio.sleep(2)
+		await mensaje.delete()
 
 
 @client.command(name='dogedice', pass_context=True)
@@ -248,7 +298,7 @@ async def dogedice(context, *, mensaje):
 async def limpiar(context, numero):
 	if context.message.author.id in constantes.ADMINISTRADORES:
 		numero = int(numero)
-
+		
 		if numero > 100:
 			await context.message.delete()
 			error_embed = discord.Embed(
@@ -260,10 +310,10 @@ async def limpiar(context, numero):
 			await asyncio.sleep(2)
 			await mensaje.delete()
 			return
-
+		
 		await context.message.delete()
 		await context.message.channel.purge(limit=numero)
-
+		
 		if numero != 1:
 			info_embed = discord.Embed(
 				title='Fui utilizado para:',
@@ -276,7 +326,7 @@ async def limpiar(context, numero):
 				description='Eliminar {} mensaje.'.format(numero),
 				color=0x00FF00
 			)
-
+		
 		mensaje = await context.message.channel.send(embed=info_embed)
 		await asyncio.sleep(2)
 		await mensaje.delete()
