edcc54c815eb1779c396168e79535eac88bc4b97
Joaquín Puche <lu4ypj@pm.me>
2020-07-22 19:11:31 -0300

    ¡VERSIÓN 1.4!
    Se mejoró la forma en la que el bot detecta si el servidor se apaga.
    Se cambió el mensaje de respuesta de $reiniciar a uno más apropiado.
    Se actualizaron los comandos de ssh.
    Se añadieron los tamaños de todos los mundos en el comando $estado.
    Se añadió el comando $latencia.
    Se cambió la forma de obtener el logo de Dogesthetic SMP.

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
+import json
 import platform
-import re
-
+import time
 import discord
 import constantes
 import asyncio
 import paramiko
 import minestat
 from discord.ext.commands import Bot
+from mcstatus import MinecraftServer
 
 client = Bot(command_prefix=constantes.PREFIX)
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-VERSION = '1.1'
+VERSION = '1.4'
 estaba_on = True
+
+
+def obtener_logo_servidor():
+	return 'https://cdn.discordapp.com/attachments/424759167845138433/735259436008079420/dogesthetic_nuevo_logo.png'
 
 
 def establecer_estaba_on(valor):
@@ -29,7 +34,7 @@ def footer_embed(context):
 	return 'Comando solicitado por {}. \nDSMPBot v{}'.format(str(context.author), VERSION)
 
 
-async def conectar_ssh(comando):
+def conectar_ssh(comando):
 	ssh.connect(constantes.SSH_HOST, port=22, username=constantes.SSH_USER, password=constantes.SSH_PASS)
 	(sshin, sshout, ssherr) = ssh.exec_command(comando)
 	resultado = sshout.read().decode('utf8').strip()
@@ -40,7 +45,7 @@ async def conectar_ssh(comando):
 async def ver_estado_servidor_loop(canal_on_off):
 	while True:
 		servidor = minestat.MineStat(constantes.IP, 25565)
-		if servidor.motd is not None:
+		if servidor.latency is not None:
 			if not obtener_estaba_on():
 				await canal_on_off.send('¡El servidor está ON!')
 				establecer_estaba_on(True)
@@ -61,44 +66,16 @@ async def on_ready():
 	print('Logueado como {}'.format(client.user))
 	print('Versión de Discord.py: {}'.format(discord.__version__))
 	print('-----------------------------------------------')
-
-
-@client.command(name='backup', pass_context=True)
-async def backup(context):
-	if context.message.author.id in constantes.PERMISOS_BACKUP:
-		try:
-			ssh.connect(constantes.SSH_HOST, port=22, username=constantes.SSH_USER, password=constantes.SSH_PASS)
-			ssh.exec_command('cp /home/DSMP_p2/world /home/backup/world -r')
-			
-			estado_embed = discord.Embed(
-				description='{} está haciendo un backup al servidor.'.format(context.author),
-				color=0x00FF00
-			)
-			
-			await context.message.channel.send(embed=estado_embed)
-		finally:
-			ssh.close()
-	else:
-		await context.message.delete()
-		error = discord.Embed(
-			title='¡Vaya!',
-			description='¡No tienes permisos para usar este comando, pequeño curioso!',
-			color=0xCC0000
-		)
-		mensaje = await context.message.channel.send(embed=error)
-		await asyncio.sleep(2)
-		await mensaje.delete()
-
+	
 
 @client.command(name='reiniciar', pass_context=True)
 async def reiniciar(context):
 	if context.message.author.id in constantes.PERMISOS_REINICIO:
 		try:
-			ssh.connect(constantes.SSH_HOST, port=22, username=constantes.SSH_USER, password=constantes.SSH_PASS)
-			ssh.exec_command('systemctl restart dogesthetic')
+			conectar_ssh('systemctl restart dsmp')
 			
 			estado_embed = discord.Embed(
-				description='{} está reiniciando el servidor.'.format(context.author),
+				description='{} reinició el servidor.'.format(context.author),
 				color=0x00FF00
 			)
 			
@@ -122,23 +99,41 @@ async def estado(context):
 	try:
 		# Obtener datos
 		servidor = minestat.MineStat(constantes.IP, 25565)
-		resultado_estado = await conectar_ssh('systemctl status dogesthetic | grep -i Active')
-		resultado_peso = await conectar_ssh("du -sh /home/DSMP_p2/world")
-		resultado_peso = re.sub("/.*$", "", resultado_peso).replace(',', '.')
+		servidor_jugadores = MinecraftServer(constantes.IP, 25565)
+		
+		estado_jugadores = servidor_jugadores.status().raw
+		
+		try:
+			resultado_peso = conectar_ssh('du -sh /opt/DSMP/*/ | sort -rh | head -3')
+			peso_end = resultado_peso.splitlines()[0].split('/')[0]
+			peso_world = resultado_peso.splitlines()[1].split('/')[0]
+			peso_nether = resultado_peso.splitlines()[2].split('/')[0]
+		except:
+			peso_end = 'ERROR'
+			peso_nether = 'ERROR'
+			peso_world = 'ERROR'
 		
 		# Mostrar datos
-		estado_servicio = 'INACTIVO'
 		estado_servidor = 'INACTIVO'
-		estado_jugadores = '0/{}'.format(servidor.max_players)
+		nombres_jugadores = ''
+		for x in range(0, servidor_jugadores.status().players.online):
+			nombres_jugadores += str(json.loads(json.dumps(estado_jugadores))['players']['sample'][x]['name'])
+			nombres_jugadores += ', '
 		
-		if 'running' in resultado_estado:
-			estado_servicio = 'ACTIVO'
+		nombres_jugadores = nombres_jugadores.rstrip(', ')
+		temp_jugadores = nombres_jugadores[::-1]
+		temp_jugadores_2 = temp_jugadores.replace(', '[::-1], ' y '[::-1], 1)
+		nombres_jugadores = temp_jugadores_2[::-1]
+		
+		estado_jugadores = '0/{}'.format(servidor.max_players)
 		
 		if servidor.version is not None:
 			estado_servidor = 'ACTIVO'
 		
-		if servidor.current_players is not None:
-			estado_jugadores = '{}/{}'.format(servidor.current_players, servidor.max_players)
+		if servidor.current_players != 0:
+			estado_jugadores = '{}/{} ({})'.format(
+				servidor.current_players, servidor.max_players, nombres_jugadores
+			)
 		
 		if estado_servidor == 'ACTIVO':
 			color_embed = 0x00FF00
@@ -146,13 +141,7 @@ async def estado(context):
 			color_embed = 0xCC0000
 		
 		estado_embed = discord.Embed(color=color_embed)
-		estado_embed.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
-		
-		estado_embed.add_field(
-			name='Estado del servicio:',
-			value=estado_servicio,
-			inline=False
-		)
+		estado_embed.set_thumbnail(url=obtener_logo_servidor())
 		
 		estado_embed.add_field(
 			name='Estado del servidor:',
@@ -161,8 +150,20 @@ async def estado(context):
 		)
 		
 		estado_embed.add_field(
-			name='Tamaño del mundo:',
-			value=resultado_peso,
+			name='Tamaño del overworld:',
+			value=peso_world,
+			inline=False
+		)
+		
+		estado_embed.add_field(
+			name='Tamaño del end:',
+			value=peso_end,
+			inline=False
+		)
+		
+		estado_embed.add_field(
+			name='Tamaño del nether:',
+			value=peso_nether,
 			inline=False
 		)
 		
@@ -180,7 +181,7 @@ async def estado(context):
 @client.command(name='reglas', pass_context=True)
 async def reglas(context):
 	reglas_embed = discord.Embed(color=0x00FF00)
-	reglas_embed.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
+	reglas_embed.set_thumbnail(url=obtener_logo_servidor())
 	reglas_embed.add_field(
 		name='Regla 1:',
 		value='No robar items ni duplicarlos.',
@@ -229,6 +230,14 @@ async def reglas(context):
 	await context.message.channel.send(embed=reglas_embed)
 
 
+@client.command(name='latencia', pass_context=True)
+async def latencia(context):
+	tiempo = time.monotonic()
+	mensaje = await context.message.channel.send("Midiendo mi latencia.")
+	ping = (time.monotonic() - tiempo) * 1000
+	await mensaje.edit(content='La latencia entre Discord y yo es de {} ms.'.format(int(ping)))
+
+
 @client.command(name='sobredsmp', pass_context=True)
 async def sobredsmp(context):
 	infodsmp = discord.Embed(
@@ -238,7 +247,7 @@ async def sobredsmp(context):
 		'formulario lo puedes encontrar en <#437781641201451010>, donde encontrarás instrucciones.',
 		color=0x00FF00
 	)
-	infodsmp.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
+	infodsmp.set_thumbnail(url=obtener_logo_servidor())
 	infodsmp.set_footer(text=footer_embed(context))
 	await context.message.channel.send(embed=infodsmp)
 
@@ -246,7 +255,7 @@ async def sobredsmp(context):
 @client.command(name='ip', pass_context=True)
 async def ip(context):
 	ip_embed = discord.Embed(color=0x00FF00)
-	ip_embed.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
+	ip_embed.set_thumbnail(url=obtener_logo_servidor())
 	ip_embed.add_field(
 		name='Para obtener la ip:',
 		value='<#483373646253916171>'
@@ -258,7 +267,7 @@ async def ip(context):
 @client.command(name='comandos', pass_context=True)
 async def comandos(context):
 	comandos_embed = discord.Embed(color=0x00FF00)
-	comandos_embed.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
+	comandos_embed.set_thumbnail(url=obtener_logo_servidor())
 	comandos_embed.add_field(
 		name='Reglas del servidor:',
 		value='$reglas',
@@ -281,7 +290,12 @@ async def comandos(context):
 	)
 	comandos_embed.add_field(
 		name='Estado del servidor:',
-		value='$estado'
+		value='$estado',
+		inline=False
+	)
+	comandos_embed.add_field(
+		name='Mi latencia:',
+		value='$latencia'
 	)
 	comandos_embed.set_footer(text=footer_embed(context))
 	await context.message.channel.send(embed=comandos_embed)
@@ -290,7 +304,7 @@ async def comandos(context):
 @client.command(name='version', pass_context=True)
 async def version(context):
 	version_embed = discord.Embed(color=0x00FF00)
-	version_embed.set_thumbnail(url='https://i.imgur.com/j39vdJs.png')
+	version_embed.set_thumbnail(url=obtener_logo_servidor())
 	version_embed.add_field(
 		name='Versión de discord.py:',
 		value=discord.__version__,
@@ -308,9 +322,13 @@ async def version(context):
 	)
 	version_embed.add_field(
 		name='Actualmente hosteado en:',
-		value='{} {}'.format(platform.system(), platform.release())
+		value='{} {}'.format(platform.system(), platform.release()),
+		inline=False
 	)
-	version_embed.add_field(name='Repositorio de GitHub:', value='<https://github.com/elJoa/DSMPBot>')
+	version_embed.add_field(
+		name='Repositorio de GitHub:',
+		value='<https://github.com/elJoa/DSMPBot>'
+	)
 	version_embed.set_footer(text=footer_embed(context))
 	await context.message.channel.send(embed=version_embed)
 
@@ -343,7 +361,7 @@ async def limpiar(context, numero):
 	if context.message.author.id in constantes.ADMINISTRADORES:
 		numero = int(numero)
 		
-		if numero > 100:
+		if numero > 400:
 			await context.message.delete()
 			error_embed = discord.Embed(
 				title='¡Vaya!',
@@ -397,6 +415,5 @@ async def limpiar_error(context):
 	mensaje = await context.message.channel.send(embed=error_embed)
 	await asyncio.sleep(2)
 	await mensaje.delete()
-
-
+	
 client.run(constantes.TOKEN)
